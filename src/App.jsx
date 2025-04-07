b6a5909df0f4aa1aac65bf9ceaaf1d42d0c118a5
gubar <endlessloop@bk.ru>
2025-04-07 13:09:17 +0400

    Добавление компонента Header

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Catalog } from './components/pages/Catalog'
 import { Basket } from './components/pages/Basket'
 import { Payment } from './components/pages/Payment'
+import { Header } from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        
         <Route path='/' element={<Catalog/>}/>
         <Route path='/basket' element={<Basket/>}/>
         <Route path='/payment' element={<Payment/>}/>
