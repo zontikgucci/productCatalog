30063da93786271b4b145441c1f03d1ef347832e
gubar <endlessloop@bk.ru>
2025-04-07 10:17:01 +0400

    переместил папку pages в папку components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import 'normalize.css';
 import './App.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Catalog } from './pages/Catalog'
-import { Basket } from './pages/Basket'
-import { Payment } from './pages/Payment'
+import { Catalog } from './components/pages/Catalog'
+import { Basket } from './components/pages/Basket'
+import { Payment } from './components/pages/Payment'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        
         <Route path='/' element={<Catalog/>}/>
         <Route path='/basket' element={<Basket/>}/>
         <Route path='/payment' element={<Payment/>}/>
